3078b03664449bf9c4d7a2018fb6a20aa6d4df60
Youusef Kelany <int.yousef@hollat.net>
2026-08-11 09:53:24 +0300

    Category-CRUD

---
+++ app/Http/Resources/Category/CategoryResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources\Category;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CategoryResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id'         => $this->id,
+            'name'       => $this->name,
+            'icon'       => $this->icon,
+            'status'     => $this->status,
+            'created_at' => $this->created_at?->format('Y-m-d H:i:s'),
+            'updated_at' => $this->updated_at?->format('Y-m-d H:i:s'),
+            'deleted_at' => $this->deleted_at?->format('Y-m-d H:i:s'),
+        ];
+    }
+}
